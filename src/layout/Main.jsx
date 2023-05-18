27ac5ac1485a7ee33188423c08f846dc33d701dc
Sagor Ahmed <developersagorahmed@gmail.com>
2023-05-18 11:47:27 +0600

    added a footer

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../Sheard/Navbar/Navbar.jsx";
 import { Outlet } from "react-router-dom";
+import Footer from "../Sheard/Footer/Footer.jsx";
 
 const Main = () => {
 	return (
 		<div>
 			<Navbar></Navbar>
 			<Outlet></Outlet>
+			<Footer></Footer>
 		</div>
 	);
 };
